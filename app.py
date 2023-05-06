2abc4d7752fc0d9a805e2a1b6d7a2717976ec527
Admini <89859783348@ya.ru>
2023-05-06 03:12:23 +0300

    kosyaki

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ from flask import Flask, render_template, request
 from configs import *
 from img_loader import Img_loader
 import json
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
+
 loader = Img_loader()
-loader.screens_path = '/home/dev/projects/crocohost/crocohost/static/screens'
+loader.screens_path = '/home/admnini/python_projs/crocohost/static/screens'
 loader.create_tree_of_screens()
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def create_translation_dict(dict_to_translate):
     return_dict = {}
@@ -27,46 +28,29 @@ def create_translation_dict(dict_to_translate):
 
     return return_dict
 
-
-@app.route('/caricatures', methods=['GET'])
-def caricatures():
-    query_params = request.args
+def get_template_attr(flag='notfisrt'):
     img_to_display = ''
     description_to_display = ''
     countries = ''
     personalities = ''
     type = ''
     topic = ''
+    loader.create_tree_of_screens()
 
+    if flag != 'first':
+        query_params = request.args.to_dict()['caricature']
+        splited_params = query_params.split('&')
+        loader.curr_year = splited_params[1].partition('=')[2]
+        loader.curr_publication = splited_params[2].partition('=')[2]
+        loader.curr_screen = splited_params[3].partition('=')[2]
 
+    next = [str(i) for i in loader.next_image()]
+    previous = [str(i) for i in loader.previous_image()]
 
-    if 'control_button' in query_params:
-        if query_params['control_button'] == 'Next':
-            loader.next_image()
-            img_to_display = os.path.join('static', 'screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-            path_to_file = os.path.join('screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-            description_to_display = loader.get_description_from_db(path_to_file)
-
-        if query_params['control_button'] == 'Previous':
-            loader.previous_image()
-            img_to_display = os.path.join('static', 'screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-            path_to_file = os.path.join('screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-            description_to_display = loader.get_description_from_db(path_to_file)
-
-    elif 'specific_img' in query_params:
-            loader.curr_year = query_params['year']
-            loader.curr_publication = query_params['publication']
-            loader.curr_screen = query_params['screen']
-            img_to_display = os.path.join('static', 'screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-            path_to_file = os.path.join('screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-            description_to_display = loader.get_description_from_db(path_to_file)
-
-    else:
-        loader.choose_first_img()
-        img_to_display = os.path.join('static', 'screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-        path_to_file = os.path.join('screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
-        description_to_display = loader.get_description_from_db(path_to_file)
-
+    img_to_display = os.path.join('static', 'screens', loader.curr_year, loader.curr_publication,
+                                  loader.curr_screen)
+    path_to_file = os.path.join('screens', loader.curr_year, loader.curr_publication, loader.curr_screen)
+    description_to_display = loader.get_description_from_db(path_to_file)
 
     if len(description_to_display) != 0:
         description_to_display = json.loads(description_to_display[0]['description'])
@@ -81,12 +65,46 @@ def caricatures():
     else:
         description_to_display = 'Изображение еще не описано'
 
-    print(img_to_display) 
 
-    return render_template('caricature.html', image=img_to_display, descriprion=description_to_display,
-        countries=countries, personalities=personalities,
-        year=loader.curr_year, publication=loader.curr_publication, name=loader.curr_screen,
-        type=type, topic=topic)
+    return {'image': img_to_display,
+            'description': description_to_display,
+            'countries': countries,
+            'personalities': personalities,
+            'year': loader.curr_year,
+            'publication': loader.curr_publication,
+            'name': loader.curr_screen,
+            'type': type,
+            'topic': topic,
+            'next': next,
+            'previous': previous}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/caricatures', methods=['GET'])
+def caricatures():
+    # case entering via link on main page
+    if len(request.args) == 0:
+        loader.choose_first_img()
+        render_dict = get_template_attr(flag='first')
+
+    else: render_dict = get_template_attr(flag='notfisrt')
+
+    return render_template('caricature.html',
+                           image=render_dict['image'],
+                           descriprion=render_dict['description'],
+                           countries=render_dict['countries'],
+                           personalities=render_dict['personalities'],
+                           year=render_dict['year'],
+                           publication=render_dict['publication'],
+                           name=render_dict['name'],
+                           type=render_dict['type'],
+                           topic=render_dict['topic'],
+                           next=render_dict['next'],
+                           previous=render_dict['previous'])
 
 
 @app.route('/tree')
--- img_loader.py
+++ img_loader.py
@@ -4,7 +4,6 @@ import pymysql
 
 HOST = 'localhost'
 USER = 'root'
-PASSWORD = 'password'
 DATABASE = 'pictures'
 
 def connect_to_db(func):
@@ -12,7 +11,6 @@ def connect_to_db(func):
         with pymysql.connect(
                 host=HOST,
                 user=USER,
-                password=PASSWORD,
                 database=DATABASE,
                 cursorclass=pymysql.cursors.DictCursor) as conn:
             with conn.cursor() as cursor:
@@ -25,99 +23,123 @@ def connect_to_db(func):
 
 
 class Img_loader:
-    def __init__(self):
-        self.curr_year = ''
-        self.curr_publication = ''
-        self.curr_screen = ''
-        self.dir_tree = {}
-        self.screens_path = ''
+        def __init__(self):
+            self.curr_year = ''
+            self.curr_publication = ''
+            self.curr_screen = ''
+            self.dir_tree = {}
+            self.screens_path = ''
 
-    @connect_to_db
-    def get_description_from_db(self, path_to_file):
-        sql = 'SELECT description FROM `data` WHERE `path_to_picture` LIKE (%s)'
-        return sql
-
-
-
-    def choose_first_img(self):
-        first_year = sorted(list(self.dir_tree.keys()), key=lambda num: int(num))[0]
-        first_publication = sorted(self.dir_tree[f'{first_year}'], key=lambda num: int(num))[0]
-        fisrt_screen = sorted(self.dir_tree[f'{first_year}'][f'{first_publication}'],
-                              key=lambda num: int(num.split('.')[0]))[0]
-        self.curr_year = first_year
-        self.curr_publication = first_publication
-        self.curr_screen = fisrt_screen
+        @connect_to_db
+        def get_description_from_db(self, path_to_file):
+            sql = 'SELECT description FROM `data` WHERE `path_to_picture` LIKE (%s)'
+            return sql
 
 
 
-    def create_tree_of_screens(self):
-        for year in sorted(os.listdir(f'{self.screens_path}')):
-            self.dir_tree[f'{year}'] = {}
-            for publication in sorted(os.listdir(f'{self.screens_path}/{year}'), key=lambda val: int(val)):
-                for screen in sorted(os.listdir(f'{self.screens_path}/{year}/{publication}')):
-                    if f'{publication}' in self.dir_tree[f'{year}'].keys():
-                        self.dir_tree[f'{year}'][f'{publication}'].append(screen)
-                    else:
-                        self.dir_tree[f'{year}'][f'{publication}'] = [screen]
+        def choose_first_img(self):
+            first_year = sorted(list(self.dir_tree.keys()), key=lambda num: int(num))[0]
+            first_publication = sorted(self.dir_tree[f'{first_year}'], key=lambda num: int(num))[0]
+            fisrt_screen = sorted(self.dir_tree[f'{first_year}'][f'{first_publication}'],
+                                  key=lambda num: int(num.split('.')[0]))[0]
 
-    def next_image(self):
-        # check of null image
-        if self.curr_screen == '':
-            return 0
-
-        current_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'],
-                                          key=lambda num: int(num.split('.')[0]))
-        current_year_list = sorted(list(self.dir_tree[f'{self.curr_year}'].keys()),
-                                   key=lambda num: int(num))
-        list_of_years = sorted(list(self.dir_tree.keys()), key=lambda num: int(num))
-
-        ### case needed next_image
-        if self.curr_screen != current_publication_list[-1]:
-            inx_of_current_screen = current_publication_list.index(self.curr_screen)
-            self.curr_screen = current_publication_list[inx_of_current_screen + 1]
-
-        ### case current screen is the last one in the publication
-        elif self.curr_screen == current_publication_list[-1] and self.curr_publication != current_year_list[-1]:
-            inx_of_current_publication = current_year_list.index(self.curr_publication)
-            self.curr_publication = current_year_list[inx_of_current_publication + 1]
-            new_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'])
-            self.curr_screen = new_publication_list[0]
+            self.curr_year = first_year
+            self.curr_publication = first_publication
+            self.curr_screen = fisrt_screen
 
 
-        ### case need to change year
-        elif self.curr_screen == current_publication_list[-1] and self.curr_publication == current_year_list[-1] and \
-                list_of_years[-1] != self.curr_year:
-            inx_current_year = list_of_years.index(self.curr_year)
-            self.curr_year = list_of_years[inx_current_year + 1]
-            self.curr_publication = sorted(self.dir_tree[f'{self.curr_year}'])[0]
-            self.curr_screen = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'])[0]
+
+        def create_tree_of_screens(self):
+            for year in sorted(os.listdir(f'{self.screens_path}')):
+                self.dir_tree[f'{year}'] = {}
+                for publication in sorted(os.listdir(f'{self.screens_path}/{year}'), key=lambda val: int(val)):
+                    for screen in sorted(os.listdir(f'{self.screens_path}/{year}/{publication}')):
+                        if f'{publication}' in self.dir_tree[f'{year}'].keys():
+                            self.dir_tree[f'{year}'][f'{publication}'].append(screen)
+                        else:
+                            self.dir_tree[f'{year}'][f'{publication}'] = [screen]
 
 
-    def previous_image(self):
-        if self.curr_screen == '':
-            return 0
-        current_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'],
-                                          key=lambda num: int(num.split('.')[0]))
-        current_year_list = sorted(list(self.dir_tree[f'{self.curr_year}'].keys()),
-                                   key=lambda num: int(num))
-        list_of_years = sorted(list(self.dir_tree.keys()), key=lambda num: int(num))
+        def next_image(self):
 
-        ### case needed rpevious image
-        if self.curr_screen != current_publication_list[0]:
-            inx_of_current_screen = current_publication_list.index(self.curr_screen)
-            self.curr_screen = current_publication_list[inx_of_current_screen - 1]
+            next_year = 0
+            next_publication = 0
+            next_screen = ''
 
-        ### case current screen is the first one in the publication
-        elif self.curr_screen == current_publication_list[0] and self.curr_publication != current_year_list[0]:
-            inx_of_current_publication = current_year_list.index(self.curr_publication)
-            self.curr_publication = current_year_list[inx_of_current_publication - 1]
-            new_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'])
-            self.curr_screen = new_publication_list[-1]
+            current_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'],
+                                              key=lambda num: int(num.split('.')[0]))
+            current_year_list = sorted(list(self.dir_tree[f'{self.curr_year}'].keys()),
+                                       key=lambda num: int(num))
+            list_of_years = sorted(list(self.dir_tree.keys()), key=lambda num: int(num))
 
-        ### case need to change year
-        elif self.curr_screen == current_publication_list[0] and self.curr_publication == current_year_list[0] and \
-                list_of_years[0] != self.curr_year:
-            inx_current_year = list_of_years.index(self.curr_year)
-            self.curr_year = list_of_years[inx_current_year - 1]
-            self.curr_publication = sorted(self.dir_tree[f'{self.curr_year}'])[-1]
-            self.curr_screen = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'])[-1]
+            ### case needed next_image
+            if self.curr_screen != current_publication_list[-1]:
+                inx_of_current_screen = current_publication_list.index(self.curr_screen)
+                next_screen = current_publication_list[inx_of_current_screen + 1]
+
+                return (self.curr_year, self.curr_publication, next_screen)
+
+            ### case current screen is the last one in the publication
+            elif self.curr_screen == current_publication_list[-1] and self.curr_publication != current_year_list[-1]:
+                inx_of_current_publication = current_year_list.index(self.curr_publication)
+                next_publication = current_year_list[inx_of_current_publication + 1]
+                new_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{next_publication}'])
+                next_screen = new_publication_list[0]
+
+                return (self.curr_year, next_publication, next_screen)
+
+
+            ### case need to change year
+            elif self.curr_screen == current_publication_list[-1] and self.curr_publication == current_year_list[-1] and \
+                    list_of_years[-1] != self.curr_year:
+                inx_current_year = list_of_years.index(self.curr_year)
+                next_year = list_of_years[inx_current_year + 1]
+                next_publication = sorted(self.dir_tree[f'{next_year}'])[0]
+                next_screen = sorted(self.dir_tree[f'{next_year}'][f'{next_publication}'])[0]
+
+                return (next_year, next_publication, next_screen)
+
+            else:
+                return (0, 0, 0)
+
+        def previous_image(self):
+            previous_year = 0
+            previous_publication = 0
+            previous_screen = ''
+
+            current_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{self.curr_publication}'],
+                                              key=lambda num: int(num.split('.')[0]))
+            current_year_list = sorted(list(self.dir_tree[f'{self.curr_year}'].keys()),
+                                       key=lambda num: int(num))
+            list_of_years = sorted(list(self.dir_tree.keys()), key=lambda num: int(num))
+
+            ### case needed rpevious image
+            if self.curr_screen != current_publication_list[0]:
+                inx_of_current_screen = current_publication_list.index(self.curr_screen)
+                previous_screen = current_publication_list[inx_of_current_screen - 1]
+
+                return (self.curr_year, self.curr_publication, previous_screen)
+
+
+            ### case current screen is the first one in the publication
+            elif self.curr_screen == current_publication_list[0] and self.curr_publication != current_year_list[0]:
+                inx_of_current_publication = current_year_list.index(self.curr_publication)
+                previous_publication = current_year_list[inx_of_current_publication - 1]
+                new_publication_list = sorted(self.dir_tree[f'{self.curr_year}'][f'{previous_publication}'])
+                previous_screen = new_publication_list[-1]
+
+                return (self.curr_year, previous_publication, previous_screen)
+
+
+            ### case need to change year
+            elif self.curr_screen == current_publication_list[0] and self.curr_publication == current_year_list[0] and \
+                    list_of_years[0] != self.curr_year:
+                inx_current_year = list_of_years.index(self.curr_year)
+                previous_year = list_of_years[inx_current_year - 1]
+                previous_publication = sorted(self.dir_tree[f'{previous_year}'])[-1]
+                previous_screen = sorted(self.dir_tree[f'{previous_year}'][f'{previous_publication}'])[-1]
+
+                return (previous_year, previous_publication, previous_screen)
+
+            else:
+                return (0, 0, 0)
